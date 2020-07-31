25c333d05e47adeff068561c3a309ec5c71ff6a7
zhouchao <810753835@qq.com>
2020-07-31 15:39:54 +0800

    TextView 扩展函数

--- app/src/main/java/com/sesameweather/android/ui/place/PlaceTest.kt
+++ app/src/main/java/com/sesameweather/android/ui/place/PlaceTest.kt
@@ -1,6 +1,41 @@
 package com.sesameweather.android.ui.place
 
+import android.text.Editable
+import android.text.TextWatcher
+
 /**
  * @author zhouchao
  * @date 2020/7/16
  */
+
+fun main() {
+    example { s: String, i: Int ->
+        println("s = $s , i = $i")
+    }
+}
+
+fun example(func: (String, Int) -> Unit) {
+
+}
+
+inline fun testAddTextChangedListener(
+    crossinline beforeTextChanged: (text: CharSequence?, start: Int, count: Int, after: Int) -> Unit = { _, _, _, _ -> },
+    crossinline onTextChanged: (text: CharSequence?, start: Int, before: Int, count: Int) -> Unit = { _, _, _, _ -> },
+    crossinline afterTextChanged: (text: Editable?) -> Unit = {}
+): TextWatcher {
+    val textWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+            afterTextChanged.invoke(s)
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            onTextChanged.invoke(s, start, count, after)
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            beforeTextChanged.invoke(s, start, before, count)
+        }
+
+    }
+    return textWatcher
+}
