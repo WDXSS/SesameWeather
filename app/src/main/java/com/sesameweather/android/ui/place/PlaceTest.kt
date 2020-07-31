bb6a52b1742c25d727b3b1730556b43695c6c469
zhouchao <810753835@qq.com>
2020-07-31 15:55:32 +0800

    TextView 扩展函数

--- app/src/main/java/com/sesameweather/android/ui/place/PlaceTest.kt
+++ app/src/main/java/com/sesameweather/android/ui/place/PlaceTest.kt
@@ -15,17 +15,19 @@ fun main() {
 }
 
 fun example(func: (String, Int) -> Unit) {
-
 }
 
 inline fun testAddTextChangedListener(
-    crossinline beforeTextChanged: (text: CharSequence?, start: Int, count: Int, after: Int) -> Unit = { _, _, _, _ -> },
+    crossinline beforeTextChanged: (text: CharSequence?, start: Int, count: Int, after: Int) -> Unit = { _, _, _, _ ->
+
+    },
     crossinline onTextChanged: (text: CharSequence?, start: Int, before: Int, count: Int) -> Unit = { _, _, _, _ -> },
     crossinline afterTextChanged: (text: Editable?) -> Unit = {}
 ): TextWatcher {
     val textWatcher = object : TextWatcher {
         override fun afterTextChanged(s: Editable?) {
             afterTextChanged.invoke(s)
+            //invoke 函数 函数类型，lambda 调用执行
         }
 
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
